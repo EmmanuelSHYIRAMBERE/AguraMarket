a8ad13c1bd0b12cf6d6e1fbc823cf476a107fd70
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2023-12-05 17:01:25 +0200

    second commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
+
 
 class User extends Authenticatable
 {
@@ -42,4 +44,25 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    
+    // protected function name(): Attribute {
+    //     return Attribute::make(
+
+    //         get: fn($value) => strtoupper($value)
+    //     ) ;
+    // }
+
+    protected function name(): Attribute
+{
+    return Attribute::make(
+        get: fn(string $value) => Str::upper($value),
+    );
+}
+
+    protected function password(): Attribute {
+        return Attribute::make(
+
+            set: fn($value) => bcrypt($value)
+        ) ;
+    }
 }
